5ea2e5841ecaa2523a4953a396ffb187ace31628
oneell <jnlee813@gmail.com>
2024-09-05 19:21:26 +0700

    first commit

---
+++ src/mahasiswa/Mahasiswa.java
@@ -0,0 +1,14 @@
+package mahasiswa;
+
+public class Mahasiswa {
+        void Membaca(){
+        System.out.println("Orang itu sedang membaca di Perpustakaan");
+    }
+        void Nyontek(){
+        System.out.println("Adik itu nyontek kepada temannya");
+    }
+        void Modifikasi(){
+        System.out.println("Paman itu sedang modifikasi program");
+    }
+    
+}
---
+++ src/mahasiswa/MahasiswaBeraksi.java
@@ -0,0 +1,16 @@
+package mahasiswa;
+
+public class MahasiswaBeraksi {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Mahasiswa MHS1 = new Mahasiswa();
+        Mahasiswa MHS2 = new Mahasiswa();
+        Mahasiswa MHS3 = new Mahasiswa();
+        
+        MHS1.Membaca();
+        MHS2.Nyontek();
+        MHS3.Modifikasi();
+    }
+    
+}
